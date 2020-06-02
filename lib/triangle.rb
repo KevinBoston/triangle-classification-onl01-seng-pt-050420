77119a5fd13e7141cc611049b617199709ea8b6f
Kevin Boston <kbos22@gmail.com>
2020-06-02 21:11:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
   def name_triangle
     if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
      :equilateral
-     elsif @side1 == @side2 && @side1 == @side3 && @side2 != @side3
+     elsif @side1 == @side2 || @side1 == @side3 || @side2 != @side3
         :isoceles
      else 
       :scalene

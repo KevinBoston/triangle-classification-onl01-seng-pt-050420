06c1399bcd72369a002c28d1db7f11446efad83a
Kevin Boston <kbos22@gmail.com>
2020-06-02 18:03:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  @@all = []
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @@all << self 
+  end
+  def kind 
+    if valid? 
+      #if math 
+        
+        
+        
+     # end
+    else raise TriangleError
+  end
+  
+    end
+    
+  def valid?
+    if (@side1 + @side2 > @side3) && (@side1 + @side3 > @side2) && (@side3 + @side2 > @side1)
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
